31e0d62255e5bfa8fb5f2980d5bc3b515e254e56
lask0064 <lask0064@morris.umn.edu>
2013-03-02 01:02:11 -0600

    Added static functions we can call for our function nodes easily.

--- EssentialsOfMetaheuristics/test/gpMethods/SingleEvalTest.groovy
+++ EssentialsOfMetaheuristics/test/gpMethods/SingleEvalTest.groovy
@@ -42,5 +42,32 @@ class SingleEvalTest extends Specification{
 			fNode2.eval(varMap1)==15
 			fNode3.eval(varMap1)==56
 	}
+	
+	def "functionNode works properly with static Functions"() {
+		when:
+			def cNode1=new ConstantNode(value:5)
+			def cNode2=new ConstantNode(value:7)
+			def cNode3=new ConstantNode(value:0)
+			def fNode1 = new FunctionNode(function:Functions.multiplication, children:[cNode1, cNode2])
+			def fNode2 = new FunctionNode(function:Functions.multiplication, children:[cNode1, cNode3])
+			def fNode3 = new FunctionNode(function:Functions.protectedMultiplication, children:[cNode1, cNode3])
+			def fNode4 = new FunctionNode(function:Functions.protectedMultiplication, children:[cNode3, cNode2])
+			def fNode5 = new FunctionNode(function:Functions.division, children:[cNode1, cNode2])
+			def fNode6 = new FunctionNode(function:Functions.division, children:[cNode1, cNode3])
+			def fNode7 = new FunctionNode(function:Functions.subtraction, children:[cNode2, cNode1])
+			def fNode8 = new FunctionNode(function:Functions.pow, children:[cNode1, cNode2])
+			def fNode9 = new FunctionNode(function:Functions.pow, children:[cNode3, cNode1])
+			
+		then:
+			fNode1.eval(varMap1) == 35
+			fNode2.eval(varMap1) == 0
+			fNode3.eval(varMap1) == 5
+			fNode4.eval(varMap1) == 7
+			fNode5.eval(varMap1) == 5 / 7
+			fNode6.eval(varMap1) == 5
+			fNode7.eval(varMap1) == 2
+			fNode8.eval(varMap1) == Math.pow(5, 7)
+			fNode9.eval(varMap1) == 1
+	}
 
 }
